7903514da2184e360be6bc809bddf5195a1ca49c
juliankap415 <juliankap415@gmail.com>
2015-09-12 07:20:12 -0500

    Se crea la interfaz

--- Registros.c
+++ Registros.c
@@ -1,10 +1,30 @@
 #include "Registros.h"
+#include <curses.h>
 
 void MostrarRegistro( uint32_t registro[])
 {
-    int i;
-    for (i=0;i<=12;i++)
+    int i,j;
+
+    start_color();
+
+    init_pair(2, COLOR_GREEN, COLOR_BLACK);
+
+
+    for (i=0;i<=3;i++)
     {
-        printf("Registro # %d: %d \n",i,registro[i]);
+        move(25+i, 2);
+        for (j=0;j<=2;j++){
+            attron(COLOR_PAIR(2));
+            printw("Registro #%.2d:",j+(i*3)+1);
+            attroff(COLOR_PAIR(2));
+
+            attron(COLOR_PAIR(1));
+            printw(" %.12u \t",registro[j+(i*3)]);
+            attroff(COLOR_PAIR(1));
+        }
     }
+
+    refresh();
 }
+
+
--- test.c
+++ test.c
@@ -1,195 +1,138 @@
 #include "Registros.h"
 #include "Instrucciones.h"
 #include "Instrucciones_desplazamiento.h"
+#include <curses.h>
 
 int main()
 {
-    uint32_t registro[12],Rd,Rn,Rm; //variables de pruebam
+    uint32_t registro[12]={1,2,3,4,5,6,7,8,9,10,11,12},Rd=50,Rn=90,Rm=80; //variables de pruebam
     int OpcionSeleccion,Banderas[4],*puntero=Banderas;
 
-    printf("Seleccione la funcion que desea probar:\n\n1:Mostrar Registro\n2:ADDS\n3:AND\n4:MOV\n5:ORR\n6:SUBS\n7:LSL\n8:LSR\n9:ROR\n10:ASR\n11:BIC\n12:MVN\n13:RSBS\n14:REV\n15:REV16\n16:CMN\n17:CMP\n18:MUL\n19:TST    \n\nOpcion: ");
-    scanf("%d",&OpcionSeleccion);
+    initscr();              //inicio del modo curses
+    curs_set(0);	        // Cursor Invisible
+    raw();
+    keypad(stdscr, TRUE);
+    noecho();
+    start_color();
+    init_pair(1, COLOR_WHITE, COLOR_BLACK);
+
+    //bkgd(COLOR_PAIR(1));
+    refresh();
+
+    border( ACS_BULLET, ACS_BULLET,
+            ACS_BULLET, ACS_BULLET,
+            ACS_BULLET, ACS_BULLET,
+            ACS_BULLET, ACS_BULLET);
+
+    attron(COLOR_PAIR(1));
+
+
+    printw("Seleccione la funcion que desea probar:\n\na:Mostrar Registro\nb:ADDS\nc:AND\nd:MOV\ne:ORR\nf:SUBS\ng:LSL\nh:LSR\ni:ROR\nj:ASR\nk:BIC\nl:MVN\nm:RSBS\nn:REV\no:REV16\np:CMN\nq:CMP\nr:MUL\ns:TST    \n\n");
+    refresh();
+    attroff(COLOR_PAIR(1));
+
+    OpcionSeleccion = getch();
+
 
     switch(OpcionSeleccion)
     {
-        case 1:
+        case 'a':
             MostrarRegistro(registro);
             break;
 
-        case 2:
-            printf("Ingrese a Rd:  ");
-            scanf("%d",&Rd);
-            printf("Ingrese a Rn:  ");
-            scanf("%d",&Rn);
-            printf("Ingrese a Rm:  ");
-            scanf("%d",&Rm);
+        case 'b':
             Rd=ADDS(Rd,Rn,Rm);          //llamado de la funcion ADDS
             BANDERAS(Rd,Rn,Rm,puntero);
             break;
 
-        case 3:
-            printf("Ingrese a Rn:  ");
-            scanf("%d",&Rn);
-            printf("Ingrese a Rm:  ");
-            scanf("%d",&Rm);
+        case 'c':
             Rd=AND(Rn,Rm);              //llamado de la funcion AND
             BANDERAS(Rd,Rn,Rm,puntero);
             break;
 
-        case 4:
-            printf("Ingrese a Rn:  ");
-            scanf("%d",&Rn);
+        case 'd':
             Rd=MOV(Rn);                 //llamado de la funcion MOV
             break;
 
-        case 5:
-            printf("Ingrese a Rn:  ");
-            scanf("%d",&Rn);
-            printf("Ingrese a Rm:  ");
-            scanf("%d",&Rm);
+        case 'e':
             Rd=ORR(Rn,Rm);              //llamado de la funcion ORR
             BANDERAS(Rd,Rn,Rm,puntero);
             break;
 
-        case 6:
-            printf("Ingrese a Rd:  ");
-            scanf("%d",&Rd);
-            printf("Ingrese a Rn:  ");
-            scanf("%d",&Rn);
-            printf("Ingrese a Rm:  ");
-            scanf("%d",&Rm);
+        case 'f':
             Rd=SUBS(Rd,Rn,Rm);           //llamado de la funcion SUBS
             BANDERAS(Rd,Rn,Rm,puntero);
             break;
 
-        case 7:
-            printf("Ingrese a Rd:  ");
-            scanf("%d",&Rd);
-            printf("Ingrese a Rn:  ");
-            scanf("%d",&Rn);
-            printf("Ingrese a Rm:  ");
-            scanf("%d",&Rm);
+        case 'g':
             Rd=LSL(Rd,Rn,Rm);           //llamado de la funcion LSL
             BANDERAS(Rd,Rn,Rm,puntero);
             break;
 
-        case 8:
-            printf("Ingrese a Rd:  ");
-            scanf("%d",&Rd);
-            printf("Ingrese a Rn:  ");
-            scanf("%d",&Rn);
-            printf("Ingrese a Rm:  ");
-            scanf("%d",&Rm);
+        case 'h':
             Rd=LSR(Rd,Rn,Rm);           //llamado de la funcion LSR
             BANDERAS(Rd,Rn,Rm,puntero);
             break;
 
-        case 9:
-            printf("Ingrese a Rn:  ");
-            scanf("%d",&Rn);
-            printf("Ingrese a Rm:  ");
-            scanf("%d",&Rm);
+        case 'i':
             Rd=ROR(Rn,Rm);           //llamado de la funcion ROR
             BANDERAS(Rd,Rn,Rm,puntero);
             break;
 
-        case 10:
-            printf("Ingrese a Rd:  ");
-            scanf("%d",&Rd);
-            printf("Ingrese a Rn:  ");
-            scanf("%d",&Rn);
-            printf("Ingrese a Rm:  ");
-            scanf("%d",&Rm);
+        case 'j':
             Rd=ASR(Rd,Rn,Rm);           //llamado de la funcion ASR
             BANDERAS(Rd,Rn,Rm,puntero);
             break;
 
-        case 11:
-            printf("Ingrese a Rd:  ");
-            scanf("%d",&Rd);
-            printf("Ingrese a Rn:  ");
-            scanf("%d",&Rn);
+        case 'k':
             Rd=BIC(Rd,Rn);              //llamado de la funcion BIC
             BANDERAS(Rd,Rn,Rm,puntero);
             break;
 
-        case 12:
-            printf("Ingrese a Rn:  ");
-            scanf("%d",&Rn);
+        case 'l':
             Rd=MVN(Rn);              //llamado de la funcion MVN
             BANDERAS(Rd,Rn,Rm,puntero);
             break;
 
-        case 13:
-            printf("Ingrese a Rn:  ");
-            scanf("%d",&Rn);
+        case 'm':
             Rd=RSB(Rn);              //llamado de la funcion RSB
             BANDERAS(Rd,Rn,Rm,puntero);
             break;
 
-        case 14:
-            printf("Ingrese a Rd:  ");
-            scanf("%d",&Rd);
-            printf("Ingrese a Rm:  ");
-            scanf("%d",&Rm);
+        case 'n':
             Rd=REV(Rd,Rm);              //llamado de la funcion REV
             BANDERAS(Rd,Rn,Rm,puntero);
             break;
 
-        case 15:
-            printf("Ingrese a Rd:  ");
-            scanf("%d",&Rd);
-            printf("Ingrese a Rm:  ");
-            scanf("%d",&Rm);
+        case 'o':
             Rd=REV16(Rd,Rm);              //llamado de la funcion REV
             BANDERAS(Rd,Rn,Rm,puntero);
             break;
 
-        case 16:
-            printf("Ingrese a Rd:  ");
-            scanf("%d",&Rd);
-            printf("Ingrese a Rn:  ");
-            scanf("%d",&Rn);
-            printf("Ingrese a Rm:  ");
-            scanf("%d",&Rm);
+        case 'p':
             CMN(Rd,Rn,Rm);          //llamado de la funcion CMN
             break;
 
-        case 17:
-            printf("Ingrese a Rd:  ");
-            scanf("%d",&Rd);
-            printf("Ingrese a Rn:  ");
-            scanf("%d",&Rn);
-            printf("Ingrese a Rm:  ");
-            scanf("%d",&Rm);
+        case 'q':
             CMP(Rd,Rn,Rm);          //llamado de la funcion CMP
             break;
 
-        case 18:
-            printf("Ingrese a Rd:  ");
-            scanf("%d",&Rd);
-            printf("Ingrese a Rn:  ");
-            scanf("%d",&Rn);
-            printf("Ingrese a Rm:  ");
-            scanf("%d",&Rm);
+        case 'r':
             Rd=MUL(Rd,Rn,Rm);          //llamado de la funcion MUL
             BANDERAS(Rd,Rn,Rm,puntero);
             break;
 
-        case 19:
-            printf("Ingrese a Rd:  ");
-            scanf("%d",&Rd);
-            printf("Ingrese a Rn:  ");
-            scanf("%d",&Rn);
-            printf("Ingrese a Rm:  ");
-            scanf("%d",&Rm);
+        case 's':
             TST(Rd,Rn,Rm);          //llamado de la funcion TST
             break;
 
     }
 
-    printf("\nSu resultado es %d\nLas banderas son: N:%d   Z:%d   C:%d   V:%d",Rd,Banderas[0],Banderas[1],Banderas[2],Banderas[3]);
+    mvprintw(30,2,"\n\nSu resultado es %d\nLas banderas son: N:%d\tZ:%d\tC:%d\tV:%d",Rd,Banderas[0],Banderas[1],Banderas[2],Banderas[3]);
+    refresh();
 
+    getch();
+    endwin();
 
     return 0;
 }
