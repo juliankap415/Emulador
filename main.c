1299ccd7b2e923d16cb6bed2448a92ea85030098
juliankap415 <juliankap415@gmail.com>
2015-09-13 20:25:07 -0500

    Se realizo la implementacion de la interfaz para los codigos que se deseen probrar

--- main.c
+++ main.c
@@ -1,155 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <curses.h>
 #include "Registros.h"
 #include "Instrucciones.h"
 #include "Instrucciones_desplazamiento.h"
 
-int main()
+int main(void)
 {
-    uint32_t registro[12],Rd,Rn,Rm; //variables de prueba
-    int OpcionSeleccion,Banderas[4],*puntero=Banderas;
+    uint32_t registro[13]={0};                  //Arreglo que contiene los registros
+    int Banderas[4]={0};                        //Arreglo que contiene las banderas
 
-    printf("Seleccione la funcion que desea probar:\n\n1:Mostrar Registro\n2:ADDS\n3:AND\n4:MOV\n5:ORR\n6:SUBS\n7:LSL\n8:LSR\n9:ROR\n10:ASR\n11:BIC\n12:MVN\n13:RSBS\n14:REV\n15:REV16    \n\nOpcion: ");
-    scanf("%d",&OpcionSeleccion);
+    initscr();	                                //Inicia modo curses
+    curs_set(0);	                            //Cursor Invisible
+    raw();			                            //Activa modo raw
+	keypad(stdscr, TRUE);   	                //Obtener F1, F2, etc
+    noecho();	            	                //No imprimir los caracteres leidos
+    start_color();	                            //Permite manejar colores
+    init_pair(1,COLOR_WHITE,COLOR_CYAN); 	    // Texto blanco, fondo cyan
+    bkgd(COLOR_PAIR(1));                        //Fondo de pantalla color cyan
 
-    switch(OpcionSeleccion)
+    mvprintw(5,47,"EMULADOR");                  //Imprime el titulo de la pantalla
+    refresh();                                  //Imprime en la pantalla
+
+    border( ACS_VLINE, ACS_VLINE,               //Poner borde a la pantalla
+            ACS_HLINE, ACS_HLINE,
+            ACS_ULCORNER, ACS_URCORNER,
+            ACS_LLCORNER, ACS_LRCORNER);
+
+    while(1)                                    //While donde se encuentran las secuencias
     {
-        case 1:
-            MostrarRegistro(registro);
-            break;
+        init_pair(1,COLOR_WHITE,COLOR_CYAN);
 
-        case 2:
-            printf("Ingrese a Rd:  ");
-            scanf("%d",&Rd);
-            printf("Ingrese a Rn:  ");
-            scanf("%d",&Rn);
-            printf("Ingrese a Rm:  ");
-            scanf("%d",&Rm);
-            Rd=ADDS(Rd,Rn,Rm);          //llamado de la funcion ADDS
-            BANDERAS(Rd,Rn,Rm,puntero);
-            break;
+        switch(registro[12])                                //Almacena la funcion para cada direccion
+        {
+            case 1:                                         //Direccion 1
+                ROR(&registro[0],1,Banderas);
+                mvprintw(20,30,"Direccion 1:\tROR\t");
+                refresh();
+                break;
 
-        case 3:
-            printf("Ingrese a Rn:  ");
-            scanf("%d",&Rn);
-            printf("Ingrese a Rm:  ");
-            scanf("%d",&Rm);
-            Rd=AND(Rn,Rm);              //llamado de la funcion AND
-            BANDERAS(Rd,Rn,Rm,puntero);
-            break;
+            case 2:                                         //Direccion 2
+                ADDS(&registro[9],registro[1],3,Banderas);
+                mvprintw(20,30,"Direccion 2:\tADDS\t");
+                refresh();
+                break;
 
-        case 4:
-            printf("Ingrese a Rn:  ");
-            scanf("%d",&Rn);
-            Rd=MOV(Rn);                 //llamado de la funcion MOV
-            break;
+            case 3:                                         //Direccion 3
+                break;
 
-        case 5:
-            printf("Ingrese a Rn:  ");
-            scanf("%d",&Rn);
-            printf("Ingrese a Rm:  ");
-            scanf("%d",&Rm);
-            Rd=ORR(Rn,Rm);              //llamado de la funcion ORR
-            BANDERAS(Rd,Rn,Rm,puntero);
-            break;
+        }
 
-        case 6:
-            printf("Ingrese a Rd:  ");
-            scanf("%d",&Rd);
-            printf("Ingrese a Rn:  ");
-            scanf("%d",&Rn);
-            printf("Ingrese a Rm:  ");
-            scanf("%d",&Rm);
-            Rd=SUBS(Rd,Rn,Rm);           //llamado de la funcion SUBS
-            BANDERAS(Rd,Rn,Rm,puntero);
-            break;
+        if (registro[12]!=0)
+        {
+            MostrarRegistro(registro);                      //Imprimimos en pantalla los valores de los registros
+            init_pair(1,COLOR_WHITE,COLOR_CYAN);
+            mvprintw(10,80,"BANDERAS");                     //Imprimimos en pantalla las banderas
+            mvprintw(12,80,"N=%d\n",Banderas[0]);
+            mvprintw(13,80,"Z=%d\n",Banderas[1]);
+            mvprintw(14,80,"C=%d\n",Banderas[2]);
+            mvprintw(15,80,"S=%d\n",Banderas[3]);
 
-        case 7:
-            printf("Ingrese a Rd:  ");
-            scanf("%d",&Rd);
-            printf("Ingrese a Rn:  ");
-            scanf("%d",&Rn);
-            printf("Ingrese a Rm:  ");
-            scanf("%d",&Rm);
-            Rd=LSL(Rd,Rn,Rm);           //llamado de la funcion LSL
-            BANDERAS(Rd,Rn,Rm,puntero);
-            break;
+            border( ACS_VLINE, ACS_VLINE,
+            ACS_HLINE, ACS_HLINE,
+            ACS_ULCORNER, ACS_URCORNER,
+            ACS_LLCORNER, ACS_LRCORNER	);
 
-        case 8:
-            printf("Ingrese a Rd:  ");
-            scanf("%d",&Rd);
-            printf("Ingrese a Rn:  ");
-            scanf("%d",&Rn);
-            printf("Ingrese a Rm:  ");
-            scanf("%d",&Rm);
-            Rd=LSR(Rd,Rn,Rm);           //llamado de la funcion LSR
-            BANDERAS(Rd,Rn,Rm,puntero);
-            break;
+            refresh();
+        }
 
-        case 9:
-            printf("Ingrese a Rn:  ");
-            scanf("%d",&Rn);
-            printf("Ingrese a Rm:  ");
-            scanf("%d",&Rm);
-            Rd=ROR(Rn,Rm);           //llamado de la funcion ROR
-            BANDERAS(Rd,Rn,Rm,puntero);
-            break;
-
-        case 10:
-            printf("Ingrese a Rd:  ");
-            scanf("%d",&Rd);
-            printf("Ingrese a Rn:  ");
-            scanf("%d",&Rn);
-            printf("Ingrese a Rm:  ");
-            scanf("%d",&Rm);
-            Rd=ASR(Rd,Rn,Rm);           //llamado de la funcion ASR
-            BANDERAS(Rd,Rn,Rm,puntero);
-            break;
-
-        case 11:
-            printf("Ingrese a Rd:  ");
-            scanf("%d",&Rd);
-            printf("Ingrese a Rn:  ");
-            scanf("%d",&Rn);
-            Rd=BIC(Rd,Rn);              //llamado de la funcion BIC
-            BANDERAS(Rd,Rn,Rm,puntero);
-            break;
-
-        case 12:
-            printf("Ingrese a Rn:  ");
-            scanf("%d",&Rn);
-            Rd=MVN(Rn);              //llamado de la funcion MVN
-            BANDERAS(Rd,Rn,Rm,puntero);
-            break;
-
-        case 13:
-            printf("Ingrese a Rn:  ");
-            scanf("%d",&Rn);
-            Rd=RSB(Rn);              //llamado de la funcion RSB
-            BANDERAS(Rd,Rn,Rm,puntero);
-            break;
-
-        case 14:
-            printf("Ingrese a Rd:  ");
-            scanf("%d",&Rd);
-            printf("Ingrese a Rm:  ");
-            scanf("%d",&Rm);
-            Rd=REV(Rd,Rm);              //llamado de la funcion REV
-            BANDERAS(Rd,Rn,Rm,puntero);
-            break;
-
-        case 15:
-            printf("Ingrese a Rd:  ");
-            scanf("%d",&Rd);
-            printf("Ingrese a Rm:  ");
-            scanf("%d",&Rm);
-            Rd=REV16(Rd,Rm);              //llamado de la funcion REV
-            BANDERAS(Rd,Rn,Rm,puntero);
-            break;
-
-
+        registro[12]+=1;
+        getch();                                            //Espera entrada del usuario
     }
 
-    printf("\nSu resultado es %d\nLas banderas son: N:%d   Z:%d   C:%d   V:%d",Rd,Banderas[0],Banderas[1],Banderas[2],Banderas[3]);
 
-
+    endwin();	                                            //Finaliza el modo curses
+    getch();
     return 0;
 }
